a90c0fa69ae3a89d4b7fc1d33c0815d41be9621e
ajosepha <ariel.isaacs@gmail.com>
2014-01-06 13:21:15 -0500

    add with questions

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ class Quiz < Sinatra::Application
   get '/' do
     @music=["Velvet Underground", "Jefferson's Airplane", "David Guetta", "Grizzly Bear", "Beethoven", "I kinda like everything!"]
     @books=["Any book on typography", "Silent Spring", "Fifty Shades of Grey", "On the Road", "The Sound and the Fury", "Catcher in the Rye"]
-    @food=["Pastries modeled after works of art", "Anything from my CSA" "Pizza after a night of parrrtyyying!", "Kale chips", "Whatever is around, when I have time to eat", "Thai food"]
-    @hometown=["New Orleans", "Portland", "Miami!", "brooklyn", "Manhattan", "home is where the heart is"]
+    @food=["Wayne Thiebaud cakes", "Anything from my CSA", "Pizza after a night of parrrtyyying!", "Kale chips", "I'm too busy working, so whatever is closest", "Thai food"]
+    @hometown=["New Orleans", "Portland", "Miami!", "brooklyn", "Manhattan", "Home is where the heart is"]
     @travel_style=["In the woods, alone", "On a volunteer trip to clean up after an oil spill", "Ibiza!", "Eco-friendly hostel", "Archeological sites", "I studied abroad once"]
-    @drink=["wine, preferably at gallery openings", "artisanal whiskey", "vanilla vodka", "PBR", "Vodka, straight", "Gin and Tonic"]
+    @drink=["Wine, preferably at gallery openings", "Artisanal whiskey", "Vanilla vodka and cherry coke", "PBR", "Vodka, straight", "Gin and Tonic"]
     @breakfast_drink=["Black Coffee", "Organic free-trade green tea", "Coffee with milk and Stevia", "Organic vanilla-chai latte", "Espresso", "Latte"]
 
 
